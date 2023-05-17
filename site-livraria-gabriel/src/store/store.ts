1679968f5c6db41069630a1d9cb258c8963bb09b
gabriel <gabrielcomputacao@outlook.com>
2023-05-17 20:55:41 -0300

    refactor: corrigindo requisições duplicadas

--- site-livraria-gabriel/src/store/store.ts
+++ site-livraria-gabriel/src/store/store.ts
@@ -1,46 +1,56 @@
-import { action,  makeObservable, observable } from "mobx";
+import { action,  computed,  makeObservable, observable } from "mobx";
 import { IListaLivros } from "../shared/service/api/livros/LivrosService";
+
  
 
     class Store {
 
         listaLivros: IListaLivros[] = [];
-        listaIdLivros: number[] = [];
+        listaIdLivrosDistintos: number[] = [];
         precoTotal: number = 0;
 
 
         constructor(){
             makeObservable(this,{
-                listaIdLivros: observable,
+                listaIdLivrosDistintos: observable,
                 listaLivros: observable,
                 precoTotal: observable,
                 addLivro: action,
                 precoTotalCalculado: action,
+                qtdLivrosDistintos: computed,
             })
         }
 
         addLivro(livro: IListaLivros){
             this.listaLivros.push(livro);
 
+            if( !this.listaIdLivrosDistintos.includes(livro.id)){
+                this.listaIdLivrosDistintos.push(livro.id)
+            }
 
         }
 
         precoTotalCalculado(){
-            
+            let contagemPreco = 0;
+
             if(this.listaLivros.length > 0 && this.listaLivros){
                 this.listaLivros.forEach( (element)=>{
                 if(element.quantidade){
-                    this.precoTotal += element.preco * element.quantidade;
+                    contagemPreco += element.preco * element.quantidade;
                 }
                 
             })
-
+                this.precoTotal = contagemPreco;
                 return  this.precoTotal;
             }else{
 
                 return  this.precoTotal;
 
             }
+        }
+
+        get qtdLivrosDistintos(){
+            return this.listaIdLivrosDistintos.length
         }
 
     }
