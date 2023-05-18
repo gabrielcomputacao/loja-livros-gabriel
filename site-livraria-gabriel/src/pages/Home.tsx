32602bd2762b32f19114e68e57afa657c661dc16
gabriel <gabrielcomputacao@outlook.com>
2023-05-18 08:00:49 -0300

    style: estilização page home

--- site-livraria-gabriel/src/pages/Home.tsx
+++ site-livraria-gabriel/src/pages/Home.tsx
@@ -1,16 +1,40 @@
-import { Box } from "@mui/material"
-import { LayoutPrincipal } from "../shared/layouts"
+import { Box, Typography } from "@mui/material";
+import { LayoutPrincipal } from "../shared/layouts";
 
-export const Home = () =>{
-    return (
-        <>
-            <LayoutPrincipal>
-                <Box>
-                        testando
-                </Box>
-
-
-            </LayoutPrincipal>
-        </>
-    )
-}
+export const Home = () => {
+  return (
+    <>
+      <LayoutPrincipal>
+        <Box display="flex" justifyContent="center" gap="1.5em" flexWrap="wrap">
+          <Box width="45%">
+                <img width="100%" src={`${process.env.PUBLIC_URL}/assets/images/livraria.jpg`} alt="Livraria Gabriel" title="Livraria Gabriel" />
+          </Box>
+          <Box width="45%">
+            <Typography
+              variant="h1"
+              fontSize="42px"
+              color="primary.main"
+              fontFamily="sans-serif"
+            >
+              Livraria Gabriel
+            </Typography>
+            <Typography variant="h2" fontSize="36px" color="secondary.main">
+              Bem-vindo à Livraria Gabriel!
+            </Typography>
+            <Typography variant="body1" marginTop="1em">
+              Na Livraria Gabriel, nossa paixão é levar o mundo da literatura
+              até você. <br /><br /> Somos uma livraria dedicada a oferecer uma vasta seleção
+              de livros, desde clássicos atemporais até os lançamentos mais
+              recentes. <br /><br /> Aqui, você encontrará um ambiente acolhedor e
+              inspirador, repleto de conhecimento e histórias esperando para
+              serem descobertas. <br /> Nossa equipe apaixonada está pronta para
+              ajudá-lo a encontrar o livro perfeito para cada momento, seja para
+              alimentar sua curiosidade, relaxar, aprender algo novo ou se
+              aventurar por mundos imaginários.
+            </Typography>
+          </Box>
+        </Box>
+      </LayoutPrincipal>
+    </>
+  );
+};
