fa764f5a461c1e62e26e306ffac5c6a71cef52a3
gabriel <gabrielcomputacao@outlook.com>
2023-05-18 23:27:36 -0300

    style: estilizando footer com redes sociais

--- site-livraria-gabriel/src/shared/components/Footer.tsx
+++ site-livraria-gabriel/src/shared/components/Footer.tsx
@@ -1,10 +1,21 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
 export const Footer = () =>{
     return ( 
-        <Box bgcolor="#292929" color="#fff" padding="1em" height="100%">
-                teste
+        <Box bgcolor="#292929" color="#fff" padding="1em" height="100%" display="flex"  justifyContent="center" gap="1em" flexWrap="wrap">
+                <Typography textAlign="center" fontSize="20px">
+                    Gabriel Henrique
+                </Typography>
+                
+                <a href="https://github.com/gabrielcomputacao/loja-livros-gabriel/tree/main/site-livraria-gabriel" target="_blank" rel="noreferrer"> 
+                    <GitHubIcon  sx={{color: '#fff', fontSize: ' 32px'}}/>
+                </a>
+                <a href="https://www.linkedin.com/in/gabriel-henrique-931163181/" target="_blank" rel="noreferrer">
+                    <LinkedInIcon sx={{color: '#fff', fontSize: ' 32px'}}/>
+                </a>
         </Box>
     )
 }
